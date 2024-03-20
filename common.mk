91a62c3e3106b8af4b22040d4425a356831ae7ea
Bruno Martins <bgcngm@gmail.com>
2024-03-20 21:27:05 +0000

    sm8550-common: Remove non-existent module from PRODUCT_PACKAGES
    Strangely, build system only complains since U QPR2.
    
    Change-Id: Icb966372233841972fa9f695cc735797c43fb71e

--- common.mk
+++ common.mk
@@ -160,7 +160,6 @@ PRODUCT_PACKAGES += \
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    vendor.qti.hardware.display.config \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
     libdisplayconfig.qti \
