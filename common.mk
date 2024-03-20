0c9bc9c02a8940e341b3ffa424897f4d6a8f6d8d
nguyenhung9xdev2024 <nguyenhung9xdev2024@gmail.com>
2024-03-20 07:19:57 +0700

    sm8550-common: update bluetooth hals

--- common.mk
+++ common.mk
@@ -109,9 +109,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth.audio-impl \
-    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
+    android.hardware.bluetooth@1.1.vendor \
+    vendor.qti.hardware.bluetooth.audio-V1-ndk.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
