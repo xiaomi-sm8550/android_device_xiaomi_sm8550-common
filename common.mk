22a2325aeeb547c70115453812d0c6dce9248925
nguyenhung9xdev2024 <nguyenhung9xdev2024@gmail.com>
2024-03-21 08:37:31 +0700

    sm8550-common: build missing hidi hal

--- common.mk
+++ common.mk
@@ -235,6 +235,7 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.allocator@1.0.vendor \
+    android.hidl.base@1.0.vendor \
     android.hidl.memory.block@1.0.vendor \
     libhidltransport.vendor \
     libhwbinder.vendor
