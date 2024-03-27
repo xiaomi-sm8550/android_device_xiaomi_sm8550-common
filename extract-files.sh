05a59efb8dd8dcdcda583356c73ee2e1e73c60f8
nguyenhung9xdev2024 <143480642+nguyenhung9xdev2024@users.noreply.github.com>
2024-03-27 14:50:32 +0700

    sm8550-common: Apply patcher

--- extract-files.sh
+++ extract-files.sh
@@ -58,35 +58,38 @@ while [ "${#}" -gt 0 ]; do
     shift
 done
 
-if [ -z "${SRC}" ]; then
-    SRC="adb"
-fi
-
 function blob_fixup() {
     case "${1}" in
-        vendor/bin/hw/android.hardware.security.keymint-service-qti | vendor/lib64/libqtikeymint.so)
-            "${PATCHELF}" --add-needed android.hardware.security.rkp-V3-ndk.so "${2}"
+        odm/etc/camera/enhance_motiontuning.xml | odm/etc/camera/night_motiontuning.xml | odm/etc/camera/motiontuning.xml | odm/overlayfs/nuwa/odm/etc/camera/enhance_motiontuning.xml |odm/overlayfs/nuwa/odm/etc/camera/night_motiontuning.xml | odm/overlayfs/nuwa/odm/etc/camera/motiontuning.xml)
+            sed -i 's/<?xml=/<?xml /g' "${2}"
+            ;;
+        odm/lib64/hw/displayfeature.default.so)
+            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
+            ;;
+        odm/lib64/hw/vendor.xiaomi.sensor.citsensorservice@2.0-impl.so | odm/overlayfs/nuwa/odm/lib64/hw/vendor.xiaomi.sensor.citsensorservice@2.0-impl.so)
+            sed -i 's/_ZN13DisplayConfig10ClientImpl13ClientImplGetENSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEPNS_14ConfigCallbackE/_ZN13DisplayConfig10ClientImpl4InitENSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEPNS_14ConfigCallbackE\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0/g' "${2}"
+            ;;
+        odm/lib64/libailab_rawhdr.so | odm/lib64/libxmi_high_dynamic_range_cdsp.so | odm/overlayfs/nuwa/odm/lib64/libailab_rawhdr.so | odm/overlayfs/nuwa/odm/lib64/libxmi_high_dynamic_range_cdsp.so)
+            "${ANDROID_ROOT}"/prebuilts/clang/host/linux-x86/clang-r450784e/bin/llvm-strip --strip-debug "${2}"
             ;;
         odm/lib64/libmt@1.3.so)
             "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
             ;;
-        odm/lib64/hw/displayfeature.default.so)
-            "${PATCHELF}" --replace-needed "libstagefright_foundation.so" "libstagefright_foundation-v33.so" "${2}"
-            ;;        
+        vendor/bin/hw/android.hardware.security.keymint-service-qti | vendor/lib64/libqtikeymint.so)
+            "${PATCHELF}" --add-needed android.hardware.security.rkp-V3-ndk.so "${2}"
+            ;;
+        vendor/bin/hw/dolbycodec2 | vendor/bin/hw/vendor.dolby.hardware.dms@2.0-service | vendor/bin/hw/vendor.dolby.media.c2@1.0-service | vendor/lib/hw/audio.primary.kalama.so | vendor/lib64/hw/audio.primary.kalama.so)
+            "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
+            ;;
         vendor/etc/init/hw/init.mi_thermald.rc|vendor/etc/init/hw/init.qcom.usb.rc|vendor/etc/init/hw/init.qti.kernel.rc)
             sed -i 's/on charger/on property:init.svc.vendor.charger=running/g' "${2}"
-            ;;
-        vendor/etc/init/init.embmssl_server.rc)
-            sed -i -n '/interface/!p' "${2}"
-            ;;
-        vendor/etc/qcril_database/upgrade/config/6.0_config.sql)
-            sed -i '/persist.vendor.radio.redir_party_num/ s/true/false/g' "${2}"
-            ;;
-        vendor/etc/vintf/manifest/c2_manifest_vendor.xml)
-            sed -ni '/dolby/!p' "${2}"
-            ;;
+            ;;    
     esac
 }
+
+if [ -z "${SRC}" ]; then
+    SRC="adb"
+fi
 
 if [ -z "${ONLY_FIRMWARE}" ] && [ -z "${ONLY_TARGET}" ]; then
     # Initialize the helper for common device
